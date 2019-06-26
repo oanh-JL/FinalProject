171218faeaedb85a5d15e84efe1100f02fd884d4
oanhptl <oanhptl@eway.vn>
2019-06-26 11:38:09 +0700

    final project initial

---
+++ src/main/groovy/internship/eway/final_project/FinalProjectApplication.groovy
@@ -0,0 +1,13 @@
+package internship.eway.final_project
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FinalProjectApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FinalProjectApplication, args)
+    }
+
+}
---
+++ src/test/groovy/internship/eway/final_project/FinalProjectApplicationTests.groovy
@@ -0,0 +1,16 @@
+package internship.eway.final_project
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
